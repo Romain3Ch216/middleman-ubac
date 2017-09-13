80cf98983ee4251ad376b11b4409df7a61d04b82
Romain Thoreau <romain.thoreau@outlook.fr>
2017-09-13 14:51:57 +0200

    Navbar + banner responsive

--- source/javascripts/navbar.js
+++ source/javascripts/navbar.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
+  $(".dropdown-toggle").dropdown();
+  if ($(window).width() < 900) {
+      $('.navbar-link').addClass('hidden');
+      $('#menu').removeClass('hidden');
+    } else {
+      $('.navbar-link').removeClass('hidden');
+      $('#menu').addClass('hidden');
+    };
   $(window).resize(function() {
     if ($(window).width() < 900) {
-      $('.navbar-links').addClass('hidden');
-      $('#creatif').removeClass('hidden');
+      $('.navbar-link').addClass('hidden');
+      $('#menu').removeClass('hidden');
     } else {
-      $('.navbar-links').removeClass('hidden');
-      $('#creatif').addClass('hidden');
+      $('.navbar-link').removeClass('hidden');
+      $('#menu').addClass('hidden');
     };
   });
 })
