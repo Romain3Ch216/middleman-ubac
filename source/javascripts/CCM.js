ed0e63623e169ea0e3d893163979f0e30e7dc035
Romain Thoreau <romain.thoreau@outlook.fr>
2017-09-17 16:33:22 +0200

    Add test

--- source/javascripts/CCM.js
+++ source/javascripts/CCM.js
@@ -1,9 +1,22 @@
 $(document).ready(function() {
   if ($(window).width() > 400) {
+    console.log('superieur');
     $('#smartphone').addClass('hidden');
     $('.CCM-global').removeClass('hidden');
   } else {
+    console.log('inferieur');
     $('#smartphone').removeClass('hidden');
     $('.CCM-global').addClass('hidden');
   }
+  $(window).resize(function() {
+    if ($(window).width() > 400) {
+      console.log('superieur');
+      $('#smartphone').addClass('hidden');
+      $('.CCM-global').removeClass('hidden');
+    } else {
+      console.log('inferieur');
+      $('#smartphone').removeClass('hidden');
+      $('.CCM-global').addClass('hidden');
+    };
+  });
 });
