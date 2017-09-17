9c086608e230086cbeddb58cfe841e30981b62ff
Romain Thoreau <romain.thoreau@outlook.fr>
2017-09-17 15:58:18 +0200

    Add bootstrap for CCM

--- source/javascripts/CCM.js
+++ source/javascripts/CCM.js
@@ -1,22 +1,6 @@
-// $(document).ready(function() {
-//   var max = Math.max($('#step-1').height(),$('#step-2').height(),$('#step-3').height(),$('#step-4').height());
-//   $('#step-1').css('height', max);
-//   $('#step-2').css('height', max);
-//   $('#step-3').css('height', max);
-//   $('#step-4').css('height', max);
-//   var diff = $('#step-4').offset().top + max - $('.CCM-global').offset().top;
-//   $('.CCM-global').css('height', diff + 50);
-//   $(window).resize(function() {
-//     height1 = $('#step-1 .step-title').height() + $('#step-1 > p').height()
-//     height2 = $('#step-2 .step-title').height() + $('#step-2 > p').height()
-//     height3 = $('#step-3 .step-title').height() + $('#step-3 > p').height()
-//     height4 = $('#step-4 .step-title').height() + $('#step-4 > p').height()
-//     var max2 = Math.max(height1, height2, height3);
-//     $('#step-1').css('height', max2 + 50);
-//     $('#step-2').css('height', max2 + 50);
-//     $('#step-3').css('height', max2 + 50);
-//     $('#step-4').css('height', max2 + 50);
-//     var diff2 = $('#step-4').offset().top + max2 - $('.CCM-global').offset().top;
-//     $('.CCM-global').css('height', diff2 + 50);
-//   });
-// });
+$(document).ready(function() {
+  if ($(window).width() < 577) {
+    $('#smartphone').removeClass('hidden');
+    $('.CCM-global').addClass('hidden');
+  }
+});
