9407f63aaabe848eac4239f78039b93c1d41d766
Romain Thoreau <romain.thoreau@outlook.fr>
2017-09-17 16:15:24 +0200

    Update

--- source/javascripts/CCM.js
+++ source/javascripts/CCM.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-  if ($(window).width() < 577) {
+  if ($(window).width() < 1000) {
     $('#smartphone').removeClass('hidden');
     $('.CCM-global').addClass('hidden');
+  } else {
+    $('#smartphone').addClass('hidden');
+    $('.CCM-global').removeClass('hidden');
   }
 });
