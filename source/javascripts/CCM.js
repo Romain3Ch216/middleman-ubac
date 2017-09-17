0655e6200fe50ce710f1a6fa33356eb090dd42cb
Romain Thoreau <romain.thoreau@outlook.fr>
2017-09-17 16:22:07 +0200

    Reverse default behavior for smartphone

--- source/javascripts/CCM.js
+++ source/javascripts/CCM.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  if ($(window).width() < 1000) {
-    $('#smartphone').removeClass('hidden');
-    $('.CCM-global').addClass('hidden');
-  } else {
+  if ($(window).width() > 400) {
     $('#smartphone').addClass('hidden');
     $('.CCM-global').removeClass('hidden');
+  } else {
+    $('#smartphone').removeClass('hidden');
+    $('.CCM-global').addClass('hidden');
   }
 });
